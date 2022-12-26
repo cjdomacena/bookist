776293632779af4bc7919feec490002b46dc6498
cjdomacena <cjdomacena@yahoo.com>
2022-12-26 18:27:09 -0500

    fix: removed unused/deleted import

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import { Red_Hat_Display } from "@next/font/google";
 import { useTheme } from "@lib/context/ThemeContext";
-import { ThemeSwitcher } from "@components/button/ThemeSwitcher/ThemeSwitcher";
-
 const inter = Red_Hat_Display({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700", "800", "900"],
