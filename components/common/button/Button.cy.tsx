f010bbf1e365c5e0111d87772b71cbc535beb96b
cjdomacena <cjdomacena@yahoo.com>
2022-12-26 18:16:51 -0500

    refactor: removed cypress template test

--- components/common/button/Button.cy.tsx
+++
@@ -1,7 +0,0 @@
-import { Button } from "./Button";
-
-describe("Button", () => {
-  it("Mounts Button Properly", () => {
-    cy.mount(<Button />);
-  });
-});
--- components/common/button/Button.tsx
+++
@@ -1,3 +0,0 @@
-export const Button = () => {
-  return <button type="button">Hello</button>;
-};
