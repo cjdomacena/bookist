cfa6dd3f360a598f803b4bbc8e410288e7cd7dc6
cjdomacena <cjdomacena@yahoo.com>
2022-12-26 18:11:06 -0500

    refactor: use forwardref for reusable input

--- components/common/input/Input.tsx
+++ components/common/input/Input.tsx
@@ -1,27 +1,34 @@
 import classNames from "classnames";
-import { HTMLInputTypeAttribute, InputHTMLAttributes } from "react";
+import { HTMLInputTypeAttribute, InputHTMLAttributes, forwardRef } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   id: string;
   label?: string;
   type: HTMLInputTypeAttribute;
+  errorMessage?: string;
 }
 
-export const Input: React.FC<InputProps> = ({ id, label, type, ...props }) => {
-  return (
-    <div className="space-y-1">
-      <label htmlFor={id} className="block text-xs dark:text-neutral-400">
-        {label}
-      </label>
-      <input
-        id={id}
-        type={type}
-        {...props}
-        className={classNames(
-          props.className,
-          "w-full max-w-md rounded bg-neutral-100 px-3 py-3   placeholder:text-neutral-600  focus:bg-neutral-50 focus:outline focus:outline-neutral-300 dark:bg-neutral-900 dark:text-neutral-300 dark:outline-neutral-700 focus:dark:bg-neutral-800"
-        )}
-      />
-    </div>
-  );
-};
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ id, label, type, errorMessage, ...props }, forwardRef) => {
+    return (
+      <div className="space-y-1">
+        <label htmlFor={id} className="block text-xs dark:text-neutral-400">
+          {label}
+        </label>
+        <input
+          id={id}
+          type={type}
+          {...props}
+          className={classNames(
+            props.className,
+            "w-full max-w-md rounded bg-neutral-100 px-3 py-3   placeholder:text-neutral-600  focus:bg-neutral-50 focus:outline focus:outline-neutral-300 dark:bg-neutral-900 dark:text-neutral-300 dark:outline-neutral-700 focus:dark:bg-neutral-800"
+          )}
+          ref={forwardRef}
+        />
+        <p className="text-xs text-red-500">{errorMessage}</p>
+      </div>
+    );
+  }
+);
+
+Input.displayName = "Input";
