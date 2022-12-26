34aae3d4d4bb2f1a5d679137dfccf1e43547821c
cjdomacena <cjdomacena@yahoo.com>
2022-12-26 18:24:17 -0500

    fix: removed unused/deleted import

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,5 +1,4 @@
 import { ThemeSwitcher } from "@components/common/button";
-import { Button } from "@components/common/button/Button";
 import { Input } from "@components/common/input/Input";
 import { AuthLayout } from "@components/common/layout/AuthLayout";
 import { RegisterForm } from "@components/ui/auth";
