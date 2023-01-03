6ee452b9f66eb3a56c04564fc58c5d1c3bc8d1ab
cjdomacena <cjdomacena@yahoo.com>
2023-01-03 16:07:34 -0500

    chore: added schema for listing

--- lib/schema/index.ts
+++ lib/schema/index.ts
@@ -10,3 +10,14 @@ export const loginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8),
 });
+
+export const listingSchema = z.object({
+  id: z.string().uuid(),
+  title: z.string().min(8),
+  description: z.string().optional(),
+  price: z.number().min(1),
+  isPublished: z.boolean().default(false),
+  isbn: z
+    .string()
+    .regex(/^(?=(?:[^0-9]*[0-9]){10}(?:(?:[^0-9]*[0-9]){3})?$)[\d-]+$/),
+});
