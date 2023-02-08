7a80c69ff35db41719dfe978fbf7635f4a3b6de5
cjdomacena <cjdomacena@yahoo.com>
2023-02-07 19:09:34 -0500

    chore: added react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,11 @@ import { Navbar } from "@components/common/header/Navbar";
 import { ThemeProvider } from "@lib/context/ThemeContext";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import { useState } from "react";
@@ -10,6 +15,7 @@ import "../styles/globals.css";
 export default function App({ Component, pageProps }: AppProps) {
   const [supabase] = useState(() => createBrowserSupabaseClient());
   const router = useRouter();
+  const [queryClient] = useState(() => new QueryClient());
 
   return (
     <ThemeProvider>
@@ -19,7 +25,11 @@ export default function App({ Component, pageProps }: AppProps) {
           supabaseClient={supabase}
           initialSession={pageProps.initialSession}
         >
-          <Component {...pageProps} />
+          <QueryClientProvider client={queryClient}>
+            <Hydrate state={pageProps.dehydratedState}>
+              <Component {...pageProps} />
+            </Hydrate>
+          </QueryClientProvider>
         </SessionContextProvider>
       </main>
     </ThemeProvider>
